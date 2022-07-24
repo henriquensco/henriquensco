75e12db82933ba687f1238dba12807a121fe7603
henriquensco <henriquensco@gmail.com>
2022-07-24 12:19:07 -0300

    Add: text about me

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -14,26 +14,53 @@
     </header>
 
     <main>
-        <section class="about-section content">
+        <section class="header-about-section content">
             <div class="align-center">
                 <div id="about-me">
                     <div>
                         <h1>Hey, I'm Henrique</h1>
                     </div>
                     <div @click="aboutMe()" id="click-about-me">
-                        <span>Sobre Mim</span>
+                        <span>About Me</span>
                     </div>
                 </div>
             </div>
         </section>
 
-        <section class="container content" id="about">
-            <div class="flex-container">
-                <div>
-                    ok!1
+        <section class="content about-section" id="about">
+            <div class="container flex-container">
+                <div class="inner-content">
+                    <div id="title">
+                        <h2>A little bit about me...</h2>
+                    </div>
+                    <div id="content">
+                        <p>
+                            Hey, there! My name is Henrique Nascimento and I'm a FullStack Developer, currently working
+                            with PHP, but in my skills are included Python, JavaScript, VueJS and Laravel.
+                        </p>
+                        <p>
+                            In my childhood I fallen in love about technology and programming. So, with a few resources
+                            that I had in the past, I decided to learn about programming on my own, going to CyberCafe
+                            once or a twice in the week.
+                        </p>
+                        <p>
+                            In the period a learn a few things, I couldint learn more becouse I had to go to the school
+                            and work to help my family.
+                        </p>
+                        <p>
+                            January of the 2021 I left my home, my family and friend to go to another State o Brazil to
+                            try find my first oportunity as a Web Developer in some Company.
+                            About six months later of studying and aplying to jobs oportunity, I got my first job in the
+                            area.
+                        </p>
+                        <p>
+                            I'm still learning a lot with real projects and studying by my self, talking with friends
+                            and sharing ideas.
+                        </p>
+                    </div>
                 </div>
-                <div>
-                    ok!2
+                <div class="inner-content">
+                    <img src="../assets/guy-reading.png" class="responsive-image" width="450px" alt="">
                 </div>
             </div>
         </section>
@@ -68,8 +95,8 @@ header {
     overflow: hidden;
 }
 
-.about-section {
-    background-color: black;
+.header-about-section {
+    background-color: #000000;
 }
 
 .flex-container {
@@ -78,6 +105,7 @@ header {
     font-weight: bold;
     max-width: 1200px;
     margin: 0 auto;
+    align-items: center;
 }
 
 .content {
@@ -122,5 +150,31 @@ header {
 #click-about-me:active {
     background-color: #161616;
     border: 2px solid #fff;
+}
+
+.about-section {
+    background-color: #ffffff;
+}
+
+.inner-content {
+    margin-top: 170px;
+}
+
+.responsive-image {
+    width: 100%;
+}
+
+#about h2 {
+    border-bottom: 4px solid #636363;
+}
+
+#about #content {
+    margin-top: 20px;
+}
+
+#about #content p {
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: normal;
+    margin: 15px 0;
 }
 </style>
